0411cb54f4d3418a31a1d6001359400917707fec
speedbuild98 <lautaropectm98@gmail.com>
2022-05-19 13:53:17 -0300

    Cambios estéticos

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -10,13 +10,15 @@ export const Characters = ({characters = []}) => {
                 <div className="card border border-primary border-3 rounded rounded-lg text-primary bg-dark mb-3" style={{minWidth: "200px", minHeight:"530px"}}>
                   <img className="card-img-top" src={item.image} alt=""/>
                   <div className='card-body text-primary'>
-                    <h5 className='card-title fw-bolder'> {item.name} </h5>
+                    <h4 className='card-title fw-bolder'> {item.name} </h4>
                     <hr/>
-                     <div className='text-white '>
-                        <p className='text-secondary fw-bold'>ID {item.id}</p>
+                     <div className='text-white text-capitalize  fw-bold'>
+                        <p className={item.status === 'Alive' ? 'text-success' : 'text-danger'}>•{item.status}</p>  
+                        <p className='text-secondary'>ID {item.id}</p>
+                      </div>
+                      <div className='text-white'>  
                         <p>Especie: {item.species}</p>
-                        <p>Ubicación: {item.location.name}</p>
-                        <p className={item.status === 'Alive' ? 'text-success' : 'text-danger'}>Status: {item.status}</p>       
+                        <p>Ubicación: {item.location.name}</p>                              
                      </div>            
                   </div>
                 </div>
