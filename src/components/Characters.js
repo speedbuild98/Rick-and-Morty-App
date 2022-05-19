b0735b75d2a77d6ed3fa3885b6d15eb6c0c8f3e1
speedbuild98 <lautaropectm98@gmail.com>
2022-05-19 13:28:13 -0300

    Se cambió p según su estado

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,23 +1,22 @@
 import React from 'react'
 
+
 export const Characters = ({characters = []}) => {
-
-
-  return (
+  return(
     <div id="caracteres" className='row'>
           {
             characters.map((item, index) => (
               <div key={index} className='col mb-4 '>
                 <div className="card border border-primary border-3 rounded rounded-lg text-primary bg-dark mb-3" style={{minWidth: "200px", minHeight:"530px"}}>
                   <img className="card-img-top" src={item.image} alt=""/>
-                  <div className='card-body fw-bold text-primary'>
-                    <h5 className='card-title'> {item.name} </h5>
+                  <div className='card-body text-primary'>
+                    <h5 className='card-title fw-bolder'> {item.name} </h5>
                     <hr/>
-                     <div className='text-white'>
-                        <p>ID {item.id}</p>
+                     <div className='text-white '>
+                        <p className='text-secondary fw-bold'>ID {item.id}</p>
                         <p>Especie: {item.species}</p>
                         <p>Ubicación: {item.location.name}</p>
-                        <p className="text-info">Status: {item.status}</p>       
+                        <p className={item.status === 'Alive' ? 'text-success' : 'text-danger'}>Status: {item.status}</p>       
                      </div>            
                   </div>
                 </div>
